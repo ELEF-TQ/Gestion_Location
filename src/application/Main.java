d8f40ecf5b600cf5ff05f5ce846ddefe9d82d66e
ELEF-TQ <tariq75aitlakhlift@gmail.com>
2023-04-02 17:50:37 +0100

    Login & SignUp

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,37 +1,25 @@
 package application;
 
+
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+public class Main extends Application {
 
-public class Main extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		launch(args);
-	}
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("hello world");
-		Button btn = new Button();
-		btn.setText("Cliquer");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-			
-			@Override
-			public void handle(ActionEvent arg0) {
-				System.out.println("hello world!");
-			}
-		});
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
-		primaryStage.setScene(new Scene(root,400,350));
-		primaryStage.show();
-	}
-
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
